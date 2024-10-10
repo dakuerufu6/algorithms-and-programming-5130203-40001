4af93e585c24914acbcf437107765b0fd935d58f
dakuerufu6 <raiwhix@gmail.com>
2024-10-11 00:05:39 +0300

    qwerty

--- week02/02/home2.cpp
+++ week02/02/home2.cpp
@@ -5,22 +5,35 @@ int main() {
 	char cont;
 	int option, a, b, cnt = 0, maxnum = 0, minnum = 111111111, sumnum = 0, productodd = 1;
 	do{
+		//MENU
+		cout << "\nSelect an option from [1-7] according to the menu:" << endl;
+		cout << "[1]. Enter a number in the range between 1000 and 999999"<<endl;
+		cout << "[2]. Print the digits of the number"<<endl;
+		cout << "[3]. Count how many digits the number has"<<endl;
+		cout << "[4]. Return the lowest and highest digits of the number"<<endl;
+		cout << "[5]. Return the sum of the digits of the number"<<endl;
+		cout << "[6]. Return the product of the odd digits"<<endl;
+		cout << "[7]. Calculate the inverted version of the number"<<endl;
+		cout << "---------------------------------------------------"<<endl;
+		//Пока не введется пункт из меню, будет продолжать запрашивать.
 		do {
 			cout << "Enter number of option (from 1 to 7): ";
 			cin >> option;
 		} while (option > 7 || option < 1);
 		cout << endl;
 		switch (option) {
+		//Задание 1.
 		case 1:
 			do {
 				cout << "Enter number (from 1000 to 999999999): ";
 				cin >> b;
 			} while (option > 999999999 || option < 1000);
 			break;
+		//Задание 2.
 		case 2:
 			cout << "Enter number: ";
 			cin >> b;
-			while (b > 1) {
+			while (b > 9) {
 				if (b / 10) {
 					cout << b % 10 << ", ";
 				}
@@ -28,6 +41,7 @@ int main() {
 			}
 			cout << b;
 			break;
+		//Задание 3.
 		case 3:
 			cout << "Enter number: ";
 			cin >> b;
@@ -40,6 +54,7 @@ int main() {
 			} while (b > 1);
 			cout << cnt;
 			break;
+		//Задание 4.
 		case 4:
 			cout << "Enter number: ";
 			cin >> b;
@@ -55,6 +70,7 @@ int main() {
 			cout << " Max = " << maxnum;
 			cout << " Min = " << minnum;
 			break;
+		//Задание 5.
 		case 5:
 			cout << "Enter number: ";
 			cin >> b;
@@ -67,6 +83,7 @@ int main() {
 			} while (b > 1);
 			cout << "Summa of numbers = " << sumnum;
 			break;
+		//Задание 6.
 		case 6:
 			cout << "Enter number: ";
 			cin >> b;
@@ -78,10 +95,11 @@ int main() {
 			} while (b > 1); 
 			cout << "Product of odd numbers is " << productodd;
 			break;
+		//Задание 7.
 		case 7:
 			cout << "Enter number: ";
 			cin >> b;
-			while (b > 1) {
+			while (b > 9) {
 				if (b / 10) {
 					cout << b % 10;
 				}
@@ -90,41 +108,12 @@ int main() {
 			cout << b;
 			break;
 		}
+		//Если напишет Y => код возобновится с начала.
 		cout << "\nDo you want continue [Y|N]? "<< endl;
 		cin>> cont;
 	}while(cont == 'y' || cont == 'Y');
+	//Иначе, закончится
 		cout << "\n The program is over!"<< endl;
 	return 0;
 
 }
-
-
-
-	/*int a, b;
-
-	do {
-		cin >> a;
-		cin >> b;
-	} while (a < 100 || a > 1000 || b < 100 || b > 1000);
-	
-	int i, mcm;
-
-	i = 2;
-	mcm = 1;
-
-	while (a != 1 && b != 1) {
-		if (a % i == 0 || b % i == 0) {
-			mcm *= i;
-
-			if (a % i == 0)
-				a /= i;
-
-			if (b % i == 0)
-				b /= i;
-		}
-		else {
-			i++;
-		}
-	}
-	cout << "MCM = " << mcm;
-	*/
